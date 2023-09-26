1eb717d681c5ab401b047c0cbaf3885b22f9931c
dequavious <21810354@sun.ac.za>
2023-09-26 12:52:49 +0200

    read config

--- assignment2/PartB/read.py
+++ assignment2/PartB/read.py
@@ -4,4 +4,12 @@ import numpy as np
 
 input_dir = Path(__file__).parent / 'input'
 enu_coords = np.loadtxt(input_dir / 'antennae.txt')
-print(enu_coords)
+
+config = {}
+with open(input_dir / 'configurations.txt', "r") as file:
+    for line in file:
+        key, *values = line.strip().split()
+        config[key] = np.array(values, dtype=np.float_) if len(values) > 1 else float(values[0])
+
+print(config)
+
