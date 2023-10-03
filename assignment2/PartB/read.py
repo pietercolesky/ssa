4bfa3e3160e969256235f442fb7ab210d27ec267
dequavious <21810354@sun.ac.za>
2023-10-03 15:23:58 +0200

    update

--- assignment2/PartB/read.py
+++ assignment2/PartB/read.py
@@ -13,9 +13,9 @@ with open(input_dir / 'configurations.json', "rb") as file:
 
 
 def get_arr(row):
-    return row["right_asc"].strip('[]').split(', '), row["declination"].strip('[]').split(', ')
+    return row["right_asc"].split(','), row["declination"].split(',')
 
 
 skymodel_df = read_csv(input_dir / 'skymodel.csv', usecols=["name", "flux", "right_asc", "declination"], delimiter=";")
 skymodel_df["right_asc"], skymodel_df["declination"] = skymodel_df[["right_asc", "declination"]].apply(get_arr, axis=1)
-print(skymodel_df)
+
