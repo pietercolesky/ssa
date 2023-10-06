ab058712041e7cdb410757715997610e378fea68
Colesky, PT, Mr [23584955@sun.ac.za] <23584955@sun.ac.za>
2023-10-06 22:20:31 +0200

    done with UV tracks

--- assignment2/PartB/test.py
+++ assignment2/PartB/test.py
@@ -17,6 +17,12 @@ def dec_to_rad(d, arc_m, arc_s):
 
     return dec_rad
 
+def lat_to_rad(d, arc_m, arc_s):
+    total_degrees = d + arc_m/60 + arc_s/3600
+    lat_rad = total_degrees * (np.pi/180)
+
+    return lat_rad
+
 def lm_coordinates(ra_0, dec_0, ra, dec):
 
     delta_ra = ra - ra_0
@@ -84,6 +90,41 @@ def imaginary_visibility(point_sources):
     plt.ylabel('v (rad^-1)')
     plt.title('Imaginary part of Visibility')
     plt.show()
+    pass
+
+def visibilities(point_sources):
+    u_range = np.linspace(-4000, 4000, 500)
+    v_range = np.linspace(-3000, 3000, 500)
+
+    real_visibilities = np.zeros((len(u_range), len(v_range)))
+    imaginary_visibilities = np.zeros((len(u_range), len(v_range)))
+
+    for v in range(len(v_range)):
+        for u in range(len(u_range)):
+            for point in point_sources:
+                imaginary_visibilities[len(v_range)-v-1,u] += -point[0] * np.sin(2 * np.pi * (point[1] * u_range[u] + point[2] * v_range[v]))
+                real_visibilities[len(v_range)-v-1,u] += point[0] * np.cos(2 * np.pi * (point[1] * u_range[u] + point[2] * v_range[v]))
+
+    fig, (ax1, ax2) = plt.subplots(1,2,figsize=(10,8))
+
+    im1 = ax1.imshow(real_visibilities, extent=(u_range.min(), u_range.max(), v_range.min(), v_range.max()), cmap = "jet", aspect='auto')
+    # ax1.colorbar(label='Magnitude of Visibilities')
+    ax1.set_title('Real part of Visibility')
+    ax1.set_xlabel('u (rad^-1)')
+    ax1.set_ylabel('v (rad^-1)')
+    cbar = fig.colorbar(im1, ax=ax1, label='Magnitude of Visibilities')
+
+    im2 = ax2.imshow(imaginary_visibilities, extent=(u_range.min(), u_range.max(), v_range.min(), v_range.max()), cmap = "jet", aspect='auto')
+    # ax1.colorbar(label='Magnitude of Visibilities')
+    ax2.set_title('Imaginary part of Visibility')
+    ax2.set_xlabel('u (rad^-1)')
+    ax2.set_ylabel('v (rad^-1)')
+    cbar = fig.colorbar(im2, ax=ax2, label='Magnitude of Visibilities')
+
+    plt.tight_layout()
+    plt.show()
+
+
     pass
 
 center_ra = center[0]
@@ -165,12 +206,87 @@ paperino_flux = paperino_df['flux'][1]
 
 
 point_sources = [[papino_flux, papino_lm[0], papino_lm[1]],[paperino_flux, paperino_lm[0], paperino_lm[1]]]
-print(point_sources)
-sky_model(point_sources)
+# print(point_sources)
+# sky_model(point_sources)
 
-real_visibility(point_sources)
-imaginary_visibility(point_sources)
+# visibilities(point_sources)
 
+# print(enu_coords)
+
+#Calculating Baseline length, 
+
+def ENU_to_XYZ(azimith, elevation, distance, latitude):
+    XYZ_coordinate = distance*np.array([np.cos(latitude)*np.sin(elevation) - np.sin(latitude)*np.cos(elevation)*np.cos(azimith),
+                               np.cos(elevation)*np.sin(azimith),
+                               np.sin(latitude)*np.sin(elevation) + np.cos(latitude)*np.cos(elevation)*np.cos(azimith)])
+    
+    return XYZ_coordinate
+
+def XYZ_to_UVW(hour_angle, declination, wavelength, xyz_coordinate):
+    matrix = (1/wavelength)*np.array([[np.sin(hour_angle), np.cos(hour_angle), 0],
+                             [-np.sin(declination)*np.cos(hour_angle), np.sin(declination)*np.sin(hour_angle), np.cos(declination)],
+                             [np.cos(declination), -np.cos(declination)*np.sin(hour_angle), np.sin(declination)]])
+    
+    uvw_coordinate = matrix.dot(xyz_coordinate)
+
+    return uvw_coordinate
+
+
+
+num_steps = config["num_steps"]
+hour_angle_range = np.linspace(config['hour_angle_range'][0], config['hour_angle_range'][1], num_steps)
+
+speed_of_light = 299792458 
+wavelength = speed_of_light/ (config['obs_freq'] * 1e9)
+
+N = len(enu_coords)
+B = (int)((N**2 - N)/2)
+
+baseline_lengths = [0.0]*B
+azimiths = [0.0]*B
+elevations = [0.0]*B
+
+xyz_coordinates = []
+
+u_m = np.zeros((N,N,num_steps),dtype=float) 
+v_m = np.zeros((N,N,num_steps),dtype=float)
+w_m = np.zeros((N,N,num_steps),dtype=float)  
+
+count = 0
+for i in range(N):
+    for j in range(i+1, N):
+        baseline_lengths[count] = np.sum((enu_coords[i] - enu_coords[j])**2)
+        azimiths[count] = np.arctan2(enu_coords[j,0] - enu_coords[i,0], enu_coords[j,1] - enu_coords[i,1])
+        elevations[count] = np.arcsin((enu_coords[j,2] - enu_coords[i,2])/baseline_lengths[count]) 
+
+        #Calculating the XYZ coordinates
+        xyz_coordinate = ENU_to_XYZ(azimiths[count], elevations[count], baseline_lengths[count], lat_to_rad(*config["lat"]))
+        #Calculating UVW coordinates for all baselines for all hourangles
+        for n in range(num_steps):
+            uvw_coordinate = XYZ_to_UVW(hour_angle_range[n],dec_to_rad(*config['declination']),wavelength,xyz_coordinate)
+            u_m[i,j,n] = uvw_coordinate[0]
+            u_m[j,i,n] = -uvw_coordinate[0]
+            v_m[i,j,n] = uvw_coordinate[1]
+            v_m[j,i,n] = -uvw_coordinate[1]
+            w_m[i,j,n] = uvw_coordinate[2]
+            w_m[j,i,n] = -uvw_coordinate[2]
+            
+            pass
+        count+=1
+
+u_max = np.max(np.abs(u_m))
+v_max = np.max(np.abs(v_m))
+
+
+for i in range(N):
+    for j in range(i+1,N):
+        u = u_m[i,j,:]
+        v = v_m[i,j,:]
+
+        plt.plot(u,v,"b")
+        plt.plot(-u,-v,"r")
+
+plt.show() 
 
 
 
