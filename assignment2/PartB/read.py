7e6ed0c23239abde817761fb26d604808289d1cb
dequavious <21810354@sun.ac.za>
2023-10-25 20:27:31 +0200

    update input

--- assignment2/PartB/read.py
+++ assignment2/PartB/read.py
@@ -9,6 +9,12 @@ from utils import deg_to_rad, hours_to_rad
 input_dir = Path(__file__).parent / 'input'
 
 
+def _extract_parts(x):
+    a, b, c = map(float, x.split(":"))
+    is_negative = a < 0
+    return a, -b if is_negative else b, -c if is_negative else c
+
+
 def _calculate_l(src):
     dec = src["dec"]
     ra_delta = src["ra_delta"]
@@ -28,17 +34,17 @@ def read_enu_coords():
 def read_config():
     with open(input_dir / 'configurations.json', "rb") as file:
         config = load(file)
-    config['lat'] = deg_to_rad(*config['lat'])
-    config['ra'] = hours_to_rad(*config['ra'])
-    config['dec'] = deg_to_rad(*config['dec'])
-    config['hour_angle_range'] = list(map(lambda h: hours_to_rad(*h), config['hour_angle_range']))
+    config['lat'] = deg_to_rad(*_extract_parts(config['lat']))
+    config['ra'] = hours_to_rad(*_extract_parts(config['ra']))
+    config['dec'] = deg_to_rad(*_extract_parts(config['dec']))
+    config['hour_angle_range'] = list(map(lambda h: hours_to_rad(*_extract_parts(h)), config['hour_angle_range']))
     return config
 
 
 def read_sky_model_df():
     df = read_csv(input_dir / 'skymodel.csv', usecols=["name", "flux", "ra", "dec"])
-    df["ra"] = df["ra"].map(lambda ra: hours_to_rad(*list(map(float, ra.split(',')))))
-    df["dec"] = df["dec"].map(lambda dec: deg_to_rad(*list(map(float, dec.split(',')))))
+    df["ra"] = df["ra"].map(lambda ra: hours_to_rad(*_extract_parts(ra)))
+    df["dec"] = df["dec"].map(lambda dec: deg_to_rad(*_extract_parts(dec)))
     field_center = df.iloc[0]
     ra_0 = field_center["ra"]
     dec_0 = field_center["dec"]
@@ -51,6 +57,6 @@ def read_sky_model_df():
 def read_img_config():
     with open(input_dir / 'image.json', "rb") as file:
         config = load(file)
-    config["cell_size"] = deg_to_rad(*config["cell_size"])
+    config["cell_size"] = deg_to_rad(*_extract_parts(config["cell_size"]))
     return config
 
